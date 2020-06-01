a42cf194d206238b2b2326f71e7c6aa3d99066ee
vlchernova <66185568+vlchernova@users.noreply.github.com>
2020-06-01 07:52:22 +0300

    site

---
+++ js/menu_js.js
@@ -0,0 +1,15 @@
+ function openNav(){
+            sideMenu.style.width = "400px";
+            sideMenu.style.opacity = "1";
+            openNavSpan.style.display = "none";
+            closeNavSpan.style.display = "block";
+        }
+
+        function closeNav(){
+            sideMenu.style.width = "0px";
+            sideMenu.style.opacity = "0";
+            closeNavSpan.style.display = "none";
+            openNavSpan.style.display = "block";
+        }
+
+
